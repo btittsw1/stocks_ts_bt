bc1b13ff458717d2b858d45b6f79c3bfa9dbc924
Braxton Tittsworth <btittsw1@vols.utk.edu>
2022-03-03 10:08:33 -0500

    Repo

---
+++ stock.R
@@ -0,0 +1,12 @@
+library(fpp3)
+stocks <- read.csv("stocks/nyse_stocks.csv")
+head(stocks)
+
+stocks$date <- as.Date(stocks$date)
+stocks <- tsibble(stocks, index = date, key = symbol)
+
+selected_stock = "AAPL"
+stocks %>%
+  filter(symbol %in% selected_stock) %>%
+  autoplot(open)
+#plan by March 7
