60248d41bce3b1d44df4fd0f6e88f57cf6766196
Braxton Tittsworth <btittsw1@vols.utk.edu>
2022-03-09 17:14:22 -0500

    Data Instructions

--- global.R
+++ global.R
@@ -1,5 +1,5 @@
 library(fpp3)
-
+#unzip data file; GitIgnore has been updated, so the data will work when unzipped
 stocks <- read.csv("nyse_stocks.csv")
 stocks$date <- as.Date(stocks$date)
 stocks <- tsibble(stocks, index = date, key = symbol)
