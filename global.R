f486b260db2280f6b48994b3d3f6e52268f1f61f
SoonerTdub <cecilw007@gmail.com>
2022-03-11 15:36:28 -0500

    change tab items

--- global.R
+++ global.R
@@ -1,4 +1,8 @@
 library(fpp3)
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(ggplot2)
 
 stocks <- read.csv("nyse_stocks.csv")
 stocks$date <- as.Date(stocks$date)
--- ui.R
+++ ui.R
@@ -1,26 +1,38 @@
-#library
-library(shiny)
-library(shinydashboard)
+sidebar<-dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard",icon=icon("dashboard")),
+    menuItem("Charts", tabName = "charts", icon = icon("th"))
+  )
+)
+
+
+body<-dashboardBody(
+  tabItems(
+    tabItem(tabName = "dashboard",
+            h2("Dashboard tab content")
+    ),
+    
+    tabItem(tabName = "charts",
+            h2("Charts tab content")
+    )
+  )
+)
 
 ui <- dashboardPage(
-    dashboardHeader(title = "Time Series Stock Analysis"),
-    dashboardSidebar(
-      sidebarMenu(
-        menuItem("Dashboard", tabName = "dashboard"),
-        menuItem("Charts", tabName = "charts", icon = icon("th"))
-      )
-    ),
-    dashboardBody(
-      fluidRow(
-        #Feature 4
-        selectInput(inputId ="select", 
-                    label = h3("Select Location"), 
-                    choices = unique(stocks$state),
-                    ),
-        plotOutput("plotted_location"),
-        verbatimTextOutput("debug")
-      )
-      
-      
-    )
+  dashboardHeader(title = "Time Series Stock Analysis"),
+  sidebar,
+  body
 )
+
+
+
+
+
+# fluidRow(
+#   #Feature 4
+#   selectInput(inputId ="select", 
+#               label = h3("Select Location"), 
+#               choices = unique(stocks$state),
+#   ),
+#   plotOutput("plotted_location"),
+#   verbatimTextOutput("debug")
