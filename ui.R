fa9ee8185072953f29fa235a8b19ca2721bfe4a5
Braxton Tittsworth <btittsw1@vols.utk.edu>
2022-03-11 18:20:50 -0500

    Update Tabs

--- ui.R
+++ ui.R
@@ -9,38 +9,41 @@ ui <- dashboardPage(
   dashboardSidebar(
     width = 150,
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard"),
-      menuItem("Chart", tabName = "charts")),
-      menuItem("Search by State", tabName = "Search", icon = icon("map")),
-      menuItem("View by Date", tabName = "Date", icon = icon("calendar"))
+      menuItem("Search by State", tabName = "state"),
+      menuItem("View by Date", tabName = "date"))
   ),
   dashboardBody(
-  tabItem(tabName = "Search",
-    fluidRow(
-    #Feature 4
-    box(plotOutput("plotted_price"), width = 6, 
-        status = "primary", title = "Closing Price Year-Over-Year", solidHeader = TRUE),
+    tabItems(
+    tabItem(tabName = "state",          box(plotOutput("plotted_price"), width = 12, 
+                                             status = "primary", 
+                                             title = "Closing Price Year-Over-Year",
+                                             solidHeader = TRUE),
+            
+                                         box(selectInput(inputId ="select_state", 
+                                             label = ("Choose Location"), 
+                                             choices = unique(stocks$state)),
+                                             status = "primary", title = "Location"),
+            
+                                         box(uiOutput("stock_dropdown"),
+                                             status = "primary", 
+                                             title = "Stock")),
     
-    box(plotOutput("plotted_volume"), width = 6, 
-        status = "primary", title = "Volume Year-Over-Year", solidHeader = TRUE),
-  
-     box(selectInput(inputId ="select_state", 
-                    label = ("Choose Location"), 
-                    choices = unique(stocks$state)
-    ), status = "primary", title = "Location"),
-    
-    #Feature 2
-    box(dateRangeInput(inputId = 'selected_date_range',
-                       label = 'Select date range:',
-                       start = min(stocks$date),
-                       end = max(stocks$date),
-                       min = min(stocks$date),
-                       max = max(stocks$date),
-    ), status = "primary", title = "Date Range"
-    )
-    ),
-    box( uiOutput("stock_dropdown"), status = "primary", title = "Stock"
-    )
+    tabItem(tabName = "date",        box(plotOutput("plotted_volume"), width = 12, 
+                                         status = "primary", 
+                                         title = "Volume Year-Over-Year", 
+                                         solidHeader = TRUE),
+            
+                                      box(dateRangeInput(inputId = 'selected_date_range',
+                                         label = 'Select date range:',
+                                         start = min(stocks$date),
+                                         end = max(stocks$date),
+                                         min = min(stocks$date),
+                                         max = max(stocks$date)), 
+                                         status = "primary",
+                                         title = "Date Range")
+      )
     )
   )
-  )
+)
+    
+            
